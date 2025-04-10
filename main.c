8bb5056785796b086e00ef36bf7b35411d5972db
pipitochka <aaakulov_3@edu.hse.ru>
2025-04-10 05:42:38 +0300

    add console application

--- main.c
+++ main.c
@@ -2,24 +2,30 @@
 #include "Source/Compiler/Syntaxer/include/syntaxer.h"
 #include "Source/Compiler/CodeGenerator/include/codeGenerator.h"
 #include "Source/Safe/Error/include/error.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-
-int main() {
-    Token* tokens = lexer("../text.in");
+int main(int argc, char** argv) {
+    if (argc > 3 || argc == 1) {
+        printf("Error: expected another amount of arguments");
+        return 1;
+    }
+    char* output_path = "out.s";
+    char* input_path = argv[1];
+    if (argc == 3) {
+        output_path = argv[2];
+    }
+    
+    Token* tokens = lexer(input_path);
     Node* q = createAST(tokens);
-    //printAST(q);
-    
-    generateCode(q, "../text.out");
-    
-    
+    generateCode(q, output_path);
     freeNode(q);
-    //printTokens(tokens);
     deleteTokens(tokens);
     
     return 0;
 }
 
-//
+
 // int main(char argc, char** argv) {
 //     
 // }
